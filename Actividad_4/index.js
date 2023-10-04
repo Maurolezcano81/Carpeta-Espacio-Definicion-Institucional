dc86a34eb1c434881b9adb459946005b57970568
Maurolezcano81 <maurolezcano111@gmail.com>
2023-10-04 13:18:28 -0300

    Actividad 4 - Corregida

--- Actividad_4/index.js
+++ Actividad_4/index.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", function (){
 
     const columnas = document.querySelectorAll('.td-body');
-
     columnas.forEach(function (columna){
-        const filas = columna.parentNode;
         columna.addEventListener("click", function(){
-            filas.classList.toggle('columnaResaltada');
+            this.classList.toggle('columnaResaltada');
         })
     });
 
     const columnasTHead = document.querySelectorAll('.th-head')
-
     columnasTHead.forEach(function(columna){
-        filas = columna.parentNode;
         columna.addEventListener("click", function(){
-            filas.classList.toggle('headerResaltado');
+            this.classList.toggle('headerResaltado')
         })
     });
 
